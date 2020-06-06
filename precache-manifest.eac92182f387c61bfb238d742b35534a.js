5d34660e09bdf07f64189fa132eab104929c44b6
Shiv Patel <patel.shiv.nil@gmail.com>
2020-06-05 19:40:55 -0700

    Updates

---
+++ precache-manifest.eac92182f387c61bfb238d742b35534a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c73131e2a290d4068485dfb3386dc1",
+    "url": "/Typing-Project/index.html"
+  },
+  {
+    "revision": "61d6195e52d48422d891",
+    "url": "/Typing-Project/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c4cd0fa2222ac7135f5",
+    "url": "/Typing-Project/static/css/main.d7c588a6.chunk.css"
+  },
+  {
+    "revision": "61d6195e52d48422d891",
+    "url": "/Typing-Project/static/js/2.7993bc74.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Typing-Project/static/js/2.7993bc74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4cd0fa2222ac7135f5",
+    "url": "/Typing-Project/static/js/main.cae7ef06.chunk.js"
+  },
+  {
+    "revision": "cf62d676364ee8424591",
+    "url": "/Typing-Project/static/js/runtime-main.f570e2a1.js"
+  }
+]);
