2751a4ba4f356fdb404590bacdeffee0963a0b3b
Shiv Patel <patel.shiv.nil@gmail.com>
2020-06-21 18:33:02 -0700

    chaning css and adding more features

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -4,31 +4,33 @@ import { Container, Col, Row, Button } from "react-bootstrap";
 import "../cssfiles/node.css";
 import "tachyons";
 import $ from "jquery";
+import { totalmem } from "os";
 
 //Global variables
 let chars: string[];
 let temp: singleNode[] = [];
 let pause: number = 0;
 let escape: boolean = false;
+let start: boolean = false;
 let wpm: number = 0;
 let times: any;
+let mistakesTemp: singleNode[] = [];
+let countOfMistakes: mistakeCount[] = [];
 
 export interface NodeProps {
-  currentWord: string;
-  finalstr: string;
   text: string;
 }
 
 export interface NodeState {
   allNodes: singleNode[];
-  final: String;
   rawArray: String[];
   currentLetter: String;
   index: number;
   wordsPerMin: number;
   timerIsOn: boolean;
   timerTime: number;
-  timerStart: number;
+  accuracyScore: number;
+  totalCharTyped: number;
 }
 
 interface singleNode {
@@ -36,33 +38,37 @@ interface singleNode {
   currentColor: number;
 }
 
+interface mistakeCount {
+  letterMistake: String;
+  countMistake: number;
+}
+
 class Node extends Component<NodeProps, NodeState> {
   state: NodeState = {
     allNodes: temp,
-    final: "",
     rawArray: [],
     currentLetter: "",
     index: 0,
     wordsPerMin: 0,
     timerIsOn: false,
+    accuracyScore: 100,
     timerTime: 0,
-    timerStart: 0,
+    totalCharTyped: 0,
   };
 
   changeColor = () => {
-    chars = Array.from(this.props.text);
     var cell = this.state.allNodes.map((each, i) => {
       if (each.currentColor === 1) {
         if (i === this.state.index) {
           if (each.letter === " ") {
             return (
-              <Col className="Error1 bg-lightest-blue pa2" md="auto" key={i}>
+              <Col className="Error1 bg-black pa2 shadow-1" md="auto" key={i}>
                 {each.letter}
               </Col>
             );
           } else {
             return (
-              <Col className="Error1 bg-lightest-blue pa1" md="auto" key={i}>
+              <Col className="Error1 bg-black pa1 shadow-1" md="auto" key={i}>
                 {each.letter}
               </Col>
             );
@@ -70,13 +76,13 @@ class Node extends Component<NodeProps, NodeState> {
         } else {
           if (each.letter === " ") {
             return (
-              <Col className="Error bg-lightest-blue pa2 bb" md="auto" key={i}>
+              <Col className="Error pa2 bb" md="auto" key={i}>
                 {each.letter}
               </Col>
             );
           } else {
             return (
-              <Col className="Error bg-lightest-blue pa1 bb" md="auto" key={i}>
+              <Col className="Error pa1 bb" md="auto" key={i}>
                 {each.letter}
               </Col>
             );
@@ -85,13 +91,13 @@ class Node extends Component<NodeProps, NodeState> {
       } else if (i === this.state.index) {
         if (each.letter === " ") {
           return (
-            <Col className="Coll1 bg-lightest-blue pa2" md="auto" key={i}>
+            <Col className="Coll1 bg-black pa2 shadow-1" md="auto" key={i}>
               {each.letter}
             </Col>
           );
         } else {
           return (
-            <Col className="Coll1 bg-lightest-blue pa1" md="auto" key={i}>
+            <Col className="Coll1 bg-black pa1 shadow-1" md="auto" key={i}>
               {each.letter}
             </Col>
           );
@@ -123,31 +129,39 @@ class Node extends Component<NodeProps, NodeState> {
       clearInterval(times);
       escape = true;
     }
-    if (escape === false) {
+    if (escape === false && start === true) {
       if (e.keyCode === 32 && e.target === document.body) {
         //Prevents page from scrolling down with spacebar
         e.preventDefault();
       }
-
       const { index } = this.state;
       this.setState({ currentLetter: e.key }); //setting the current typed key value
-      console.log("current Letter", this.state.currentLetter);
-      this.setState({ final: this.state.final.concat(e.key) });
       if (this.state.index < this.state.allNodes.length) {
         if (this.state.currentLetter !== "Shift") {
-          if (this.state.currentLetter === this.state.allNodes[index].letter) {
+          this.setState({ totalCharTyped: this.state.totalCharTyped + 1 });
+          if (
+            this.state.currentLetter ===
+            this.state.allNodes[this.state.index].letter
+          ) {
             //If the current letter typed matches the proper index in the array, it will increment the index (cursor)
             this.setState({ index: index + 1 });
           } else {
             let status: singleNode[] = Object.assign({}, this.state.allNodes); //If it does not match, it will set the currentColor to red, meaning the letter was typed in incorrectly.
-            status[index].currentColor = 1; //currentColor 0 means it was typed corrently and 1 means it was typed incorrectly
+            status[this.state.index].currentColor = 1; //currentColor 0 means it was typed corrently and 1 means it was typed incorrectly
             temp = status;
           }
+        }
 
-          if (this.state.index >= 5) {
-            wpm = Math.round(index / 5 / (this.state.timerTime / 60));
-            this.setState({ wordsPerMin: wpm });
-          }
+        //Calculating the socres to display
+        if (index >= 5) {
+          wpm = Math.round(index / 5 / (this.state.timerTime / 60));
+          this.setState({ wordsPerMin: wpm });
+          let AccScore = Math.round(
+            ((this.state.index - mistakesTemp.length) /
+              this.state.totalCharTyped) *
+              100
+          );
+          this.setState({ accuracyScore: AccScore });
         }
       } else {
         this.reset();
@@ -155,11 +169,86 @@ class Node extends Component<NodeProps, NodeState> {
     }
   };
 
+  filterErrors = () => {
+    countOfMistakes = [];
+    let returnStatement;
+    mistakesTemp = this.state.allNodes.filter(
+      (nodes) => nodes.currentColor > 0
+    );
+    mistakesTemp.map((each) => {
+      if (countOfMistakes.some((i) => i.letterMistake === each.letter)) {
+        let i;
+        for (i = 0; i < countOfMistakes.length; i++) {
+          if (countOfMistakes[i].letterMistake === each.letter) {
+            countOfMistakes[i].countMistake =
+              countOfMistakes[i].countMistake + 1;
+          }
+        }
+      } else {
+        const tempNode = {
+          letterMistake: each.letter,
+          countMistake: 1,
+        };
+        countOfMistakes.push(tempNode);
+      }
+    });
+    if (mistakesTemp.length !== 0) {
+      returnStatement = (
+        <div>
+          <Row className="justify-content-md-center">
+            <Col className="text-center bg-lightest-blue b bb" md="3">
+              Errors:
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col className="bg-lightest-blue" md="3">
+              <ul>{this.returnErrors()}</ul>
+            </Col>
+          </Row>
+        </div>
+      );
+    } else {
+      returnStatement = <div></div>;
+    }
+    return returnStatement;
+  };
+
+  returnErrors = () => {
+    var tempReturn = countOfMistakes.map((nodes, key) => {
+      if (nodes.letterMistake === " ") {
+        return (
+          <li key={key}>
+            {"Space"}: {nodes.countMistake}
+          </li>
+        );
+      } else if (nodes.letterMistake === ",") {
+        return (
+          <li key={key}>
+            {"Comma"}: {nodes.countMistake}
+          </li>
+        );
+      } else if (nodes.letterMistake === ".") {
+        return (
+          <li key={key}>
+            {"Period"}: {nodes.countMistake}
+          </li>
+        );
+      } else {
+        return (
+          <li key={key}>
+            {nodes.letterMistake}: {nodes.countMistake}
+          </li>
+        );
+      }
+    });
+    return tempReturn;
+  };
+
   start = () => {
     escape = false;
+    start = true;
     $(".startButton").prop("disabled", true);
-    this.setState({ rawArray: chars });
-    console.log(this.state.allNodes);
+    chars = Array.from(this.props.text);
     if (pause === 0) {
       chars.map((each, i) => {
         const nodetest = {
@@ -184,6 +273,7 @@ class Node extends Component<NodeProps, NodeState> {
   reset = () => {
     $(".resetButton").prop("disabled", true);
     temp = [];
+    countOfMistakes = [];
     this.setState({ index: 0 });
     pause = 0;
     this.setState({ allNodes: temp });
@@ -204,7 +294,7 @@ class Node extends Component<NodeProps, NodeState> {
   render() {
     return (
       <Container>
-        <Row className="justify-content-md-center bg-lightest-blue">
+        <Row className="bg-lightest-blue topContainer">
           <Col className="text-center">
             <p>Timer: {this.state.timerTime}</p>
           </Col>
@@ -212,10 +302,10 @@ class Node extends Component<NodeProps, NodeState> {
             <p>Words per minute: {this.state.wordsPerMin}</p>
           </Col>
           <Col className="text-center">
-            <p>Characters typed: {this.state.index}</p>
+            <p>Accuracy Score: {this.state.accuracyScore}%</p>
           </Col>
         </Row>
-        <Row className="justify-content-md-center ba bg-lightest-blue">
+        <Row className="justify-content-md-center bg-lightest-blue bottomContainer">
           {this.changeColor()}
         </Row>
         <Row>
@@ -226,7 +316,7 @@ class Node extends Component<NodeProps, NodeState> {
           <Col></Col>
           <Col className="text-center">
             <Button
-              variant="outline-primary"
+              variant="warning"
               className="startButton"
               onClick={this.start}
             >
@@ -234,11 +324,11 @@ class Node extends Component<NodeProps, NodeState> {
             </Button>
           </Col>
           <Col className="text-center">
-            <h6>Press Escape key to pause</h6>
+            <h6 className="yellow">Press Escape key to pause</h6>
           </Col>
           <Col className="text-center">
             <Button
-              variant="outline-primary"
+              variant="warning"
               className="resetButton"
               onClick={this.reset}
             >
@@ -248,6 +338,12 @@ class Node extends Component<NodeProps, NodeState> {
           <Col></Col>
           <Col></Col>
         </Row>
+        <Row>
+          <br></br>
+        </Row>
+        <Container className="justify-content-md-center">
+          {this.filterErrors()}
+        </Container>
       </Container>
     );
   }
--- src/components/typing.tsx
+++ src/components/typing.tsx
@@ -7,23 +7,21 @@ import { Container } from "react-bootstrap";
 class Typing extends Component {
   state = {
     text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
-    currentWord: "",
-    finalstr: "",
+      "Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
   };
 
   render() {
-    const { currentWord, finalstr, text } = this.state;
+    const { text } = this.state;
     return (
-      <div>
-        <Container fluid className="text-center bg-lightest-blue">
+      <Container className="hero-image" fluid>
+        <Container className="text-center bg-lightest-blue topContainer" fluid>
           <br></br>
           <h4>Welcome to typing speed practice website!</h4>
           <br className=" bg-lightest-blue"></br>
         </Container>
         <br></br>
-        <Node currentWord={currentWord} finalstr={finalstr} text={text} />
-      </div>
+        <Node text={text} />
+      </Container>
     );
   }
 }
